3eecf80d741433885da3695c055988ec06271a88
Slawoj6666 <slawoj6666@op.pl>
2024-09-15 21:23:20 +0200

    added basic logic

--- project_projoppr/main.cpp
+++ project_projoppr/main.cpp
@@ -1,4 +1,13 @@
 #include <raylib.h>
 int main() {
+	InitWindow(450, 450, "test");
+	while (!WindowShouldClose()) {
+
+
+		BeginDrawing();
+		ClearBackground(RAYWHITE);
+		DrawText("test",10,50,100,LIGHTGRAY);
+		EndDrawing();
+	}
 	return 0;
 }
