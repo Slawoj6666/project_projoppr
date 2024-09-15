0bcb070c574246fdcc232fff185bdba8bdea6871
Slawoj6666 <slawoj6666@op.pl>
2024-09-15 21:24:51 +0200

    comments

--- project_projoppr/main.cpp
+++ project_projoppr/main.cpp
@@ -1,12 +1,20 @@
 #include <raylib.h>
+
 int main() {
 	InitWindow(450, 450, "test");
 	while (!WindowShouldClose()) {
+		//update here
 
 
+
+
+		//draw elemnts here
 		BeginDrawing();
+
 		ClearBackground(RAYWHITE);
 		DrawText("test",10,50,100,LIGHTGRAY);
+
+		//end of drawing
 		EndDrawing();
 	}
 	return 0;
