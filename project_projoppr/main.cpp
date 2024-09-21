d5bfba9683f6656d5f68a73cf041a57425e422fa
Slawoj6666 <slawoj6666@op.pl>
2024-09-21 20:24:51 +0200

    .

--- project_projoppr/main.cpp
+++ project_projoppr/main.cpp
@@ -3,20 +3,18 @@
 
 
 
+
+
+
 int main() {
 	InitWindow(1080, 1000, "Projekt");
 
 	while (!WindowShouldClose()) {
 		//update here
-
-
-
 		//draw elemnts here
 		BeginDrawing();
 		ClearBackground(RAYWHITE);
 		renderMenu(MenuButtons);
-
-				
 		//end of drawing
 		EndDrawing();
 	}
