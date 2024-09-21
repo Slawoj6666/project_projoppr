5065743ba182dfb172bb93ff3f67401040ec28c5
Slawoj6666 <slawoj6666@op.pl>
2024-09-21 16:38:17 +0200

    .

--- project_projoppr/main.cpp
+++ project_projoppr/main.cpp
@@ -1,17 +1,19 @@
 #include <raylib.h>
 
 int main() {
-	InitWindow(450, 450, "test");
+	InitWindow(1920, 1080, "test");
+	Image img = LoadImage("data/Image.png");
+	Texture2D tex = LoadTextureFromImage(img);
 	while (!WindowShouldClose()) {
 		//update here
-
+		
 
 
 
 		//draw elemnts here
 		BeginDrawing();
-
 		ClearBackground(RAYWHITE);
+		DrawTexture(tex,0,0,RAYWHITE);
 		DrawText("test",10,50,100,LIGHTGRAY);
 
 		//end of drawing
