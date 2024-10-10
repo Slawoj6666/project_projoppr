8e458485b0d96997970c15633867dade6bb7b1bb
Slawoj6666 <slawoj6666@op.pl>
2024-10-10 21:55:16 +0200

    test

--- project_projoppr/main.cpp
+++ project_projoppr/main.cpp
@@ -1,49 +1,42 @@
-#include <raylib.h>
+#include "CLucene.h"
 #include "menu/menu.h"
-#include <iostream>
+#include <raylib.h>
 
 int screen = 1;
 
 buttons MenuButtons[4] = {
-{50,050,100,100,1,0.5,1,{GRAY},"play",2},
-{50,175,100,100,1,0.5,1,{GRAY},"author",3},
-{50,300,100,100,1,0.5,1,{GRAY},"quit",4},
+    {50, 050, 100, 100, 1, 0.5, 1, {GRAY}, "play", 2},
+    {50, 175, 100, 100, 1, 0.5, 1, {GRAY}, "author", 3},
+    {50, 300, 100, 100, 1, 0.5, 1, {GRAY}, "quit", 4},
 };
 
-
-
-
 int main() {
-	InitWindow(600, 800, "Projekt");
-	Vector2 MousePoint = { 0.0f,0.0f };
+  InitWindow(600, 800, "Projekt");
+  Vector2 MousePoint = {0.0f, 0.0f};
 
+  while (!WindowShouldClose()) {
+    // update here
+    MousePoint = GetMousePosition();
 
-	while (!WindowShouldClose()) {
-		//update here
-		MousePoint = GetMousePosition();
+    // draw elements here
+    BeginDrawing();
+    switch (screen) {
+    case 1:
+      renderMenu(MenuButtons);
+      Menucollision(MenuButtons, MousePoint);
+      break;
+    case 2:
 
+      break;
+    case 3:
 
-
-		//draw elements here
-		BeginDrawing();
-		switch (screen)
-		{
-		case 1:
-			renderMenu(MenuButtons);
-			Menucollision(MenuButtons, MousePoint);
-			break;
-		case 2:
-
-			break;
-		case 3:
-
-			break;
-		case 4:
-			CloseWindow();
-			break;
-		}
-		ClearBackground(RAYWHITE);
-		EndDrawing();
-	}
-	return 0;
+      break;
+    case 4:
+      CloseWindow();
+      break;
+    }
+    ClearBackground(RAYWHITE);
+    EndDrawing();
+  }
+  return 0;
 }
