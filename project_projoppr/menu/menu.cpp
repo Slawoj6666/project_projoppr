ebfc5f76fb3f53c22f549fb436883c0b7e286977
Slawoj6666 <slawoj6666@op.pl>
2024-10-12 12:12:03 +0200

    naprawa logiki menu

--- project_projoppr/menu/menu.cpp
+++ project_projoppr/menu/menu.cpp
@@ -1,8 +1,7 @@
 #include "menu.h"
-#include <array>
 #include <iostream>
 
-void renderMenu(std::array<buttons, 3> button) {
+void renderMenu(buttons *button) {
 
   for (int i = 0; i < sizeof(button); i++) {
     DrawRectangleRoundedLines(button[i].rec, button[i].roundness,
@@ -18,7 +17,7 @@ void renderMenu(std::array<buttons, 3> button) {
   }
 };
 
-void Menucollision(buttons button, Vector2 rec) {
+void Menucollision(buttons *button, Vector2 rec) {
   for (size_t i = 0; i < sizeof(button); i++) {
     if (IsMouseButtonPressed(MOUSE_BUTTON_LEFT) &&
         CheckCollisionPointRec(rec, button[i].rec)) {
