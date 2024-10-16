84c6a154facf656c8dae0a0d7fe5654521a8f7c0
Slawoj6666 <slawoj6666@op.pl>
2024-10-16 20:23:57 +0200

    dodano foramtowanie

--- project_projoppr/menu/menu.cpp
+++ project_projoppr/menu/menu.cpp
@@ -2,14 +2,11 @@
 // Funkcja rysuje przyciski z podanego structa buttons
 // Nie podawać pointera ponieważ wtedy Funkcja nie działa
 void DrawMenu(buttons *button) {
-
   for (int i = 0; i < sizeof(button); i++) {
     DrawRectangleRoundedLines(button[i].rec, button[i].roundness,
                               button[i].segments, button[i].thick,
                               button[i].color);
-
     Vector2 textspac = MeasureTextEx(GetFontDefault(), button[i].text, 1, 1);
-
     DrawText(button[i].text,
              button[i].rec.x + button[i].rec.width / 2 - textspac.x - 6,
              button[i].rec.y + button[i].rec.width / 2 - textspac.y - 5, 2,
