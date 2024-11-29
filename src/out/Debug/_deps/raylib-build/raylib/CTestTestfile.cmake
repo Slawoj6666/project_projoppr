cc44e57db94964b72d830ec85e768db753f319f4
Sławomir Pawlus <slawoj6666@op.pl>
2024-11-29 17:48:17 +0100

    Delete src/out/Debug directory

--- src/out/Debug/_deps/raylib-build/raylib/CTestTestfile.cmake
+++
@@ -1,7 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/slaw/prog/project_projoppr/project_projoppr/out/Debug/_deps/raylib-src/src
-# Build directory: /home/slaw/prog/project_projoppr/project_projoppr/out/Debug/_deps/raylib-build/raylib
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-subdirs("external/glfw")
